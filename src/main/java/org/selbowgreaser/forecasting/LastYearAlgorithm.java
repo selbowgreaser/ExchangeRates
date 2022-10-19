7ed956bbf61a9e8e95cb5eb8315d9b024374680e
selbowgreaser <selbowgreaser@gmail.com>
2022-10-20 00:41:00 +0300

    The last year algorithm is reduced to the required form

--- src/main/java/org/selbowgreaser/forecasting/LastYearAlgorithm.java
+++ src/main/java/org/selbowgreaser/forecasting/LastYearAlgorithm.java
@@ -1,8 +1,8 @@
 package org.selbowgreaser.forecasting;
 
 import org.selbowgreaser.data.ExchangeRate;
+import org.selbowgreaser.data.ExchangeRateDao;
 import org.selbowgreaser.data.PredictedExchangeRate;
-import org.selbowgreaser.data.extractor.DataExtractorForLastYearAlgorithm;
 import org.selbowgreaser.request.RequestResult;
 import org.selbowgreaser.request.parameters.Algorithm;
 import org.selbowgreaser.request.parameters.Currency;
@@ -14,18 +14,17 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LastYearAlgorithm implements ForecastingAlgorithm {
-    private final Currency currency;
-    private final List<LocalDate> dates;
     public static final int SCALE = 2;
+    private final ExchangeRateDao exchangeRateDao;
 
-    public LastYearAlgorithm(Currency currency, List<LocalDate> dates) {
-        this.currency = currency;
-        this.dates = dates;
+    public LastYearAlgorithm(ExchangeRateDao exchangeRateDao) {
+        this.exchangeRateDao = exchangeRateDao;
     }
 
+
     @Override
-    public RequestResult forecast() {
-        List<ExchangeRate> exchangeRates = new DataExtractorForLastYearAlgorithm(currency, dates).extractData();
+    public RequestResult forecast(Currency currency, List<LocalDate> dates) {
+        List<ExchangeRate> exchangeRates = exchangeRateDao.getDataForLastYearAlgorithm(currency, dates);
         List<PredictedExchangeRate> predictedExchangeRates = IntStream.range(0, dates.size())
                 .mapToObj(i -> new PredictedExchangeRate(dates.get(i),
                         exchangeRates.get(i).getExchangeRate().divide(exchangeRates.get(i).getDenomination(), SCALE, RoundingMode.HALF_UP)))
