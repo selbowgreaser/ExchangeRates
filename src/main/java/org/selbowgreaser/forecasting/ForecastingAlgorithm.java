3d4994b9932fa02140981fcc43262f6024b27eb7
selbowgreaser <selbowgreaser@gmail.com>
2022-10-20 00:46:09 +0300

    Improved the interface for algorithm

--- src/main/java/org/selbowgreaser/forecasting/ForecastingAlgorithm.java
+++ src/main/java/org/selbowgreaser/forecasting/ForecastingAlgorithm.java
@@ -1,7 +1,11 @@
 package org.selbowgreaser.forecasting;
 
 import org.selbowgreaser.request.RequestResult;
+import org.selbowgreaser.request.parameters.Currency;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ForecastingAlgorithm {
-    RequestResult forecast();
+    RequestResult forecast(Currency currency, List<LocalDate> dates);
 }
