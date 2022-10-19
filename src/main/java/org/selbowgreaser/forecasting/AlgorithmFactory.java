31693219813153a0272f83df0df06671c3663379
selbowgreaser <selbowgreaser@gmail.com>
2022-10-20 00:45:44 +0300

    Improved factory for algorithms

--- src/main/java/org/selbowgreaser/forecasting/AlgorithmFactory.java
+++ src/main/java/org/selbowgreaser/forecasting/AlgorithmFactory.java
@@ -1,18 +1,26 @@
 package org.selbowgreaser.forecasting;
 
+import org.selbowgreaser.data.ExchangeRateDao;
 import org.selbowgreaser.request.parameters.Algorithm;
 
+import java.util.Random;
+
 public class AlgorithmFactory {
+    private final ExchangeRateDao exchangeRateDao;
+    private final Random random;
+
+    public AlgorithmFactory(ExchangeRateDao exchangeRateDao, Random random) {
+        this.exchangeRateDao = exchangeRateDao;
+        this.random = random;
+    }
+
     public ForecastingAlgorithm createAlgorithm(Algorithm algorithm) {
         if (algorithm.equals(Algorithm.MYSTICAL)) {
-            return new MysticalAlgorithm();
+            return new MysticalAlgorithm(exchangeRateDao, random);
         } else if (algorithm.equals(Algorithm.LAST_YEAR)) {
-            return new LastYearAlgorithm();
-        } else if (algorithm.equals(Algorithm.LINEAR_REGRESSION)) {
-            //todo: заменить на создание LinearRegressionAlgorithm
-            return null;
+            return new LastYearAlgorithm(exchangeRateDao);
+        } else {
+            return new LinearRegressionAlgorithm(exchangeRateDao);
         }
-        //todo: заменить на создание AverageAlgorithm
-        return null;
     }
 }
